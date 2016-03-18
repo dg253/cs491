5617e3a2d31147f9b2aaeb8404f45faabe01f465
yazman12 <yazman12@gmail.com>
2016-03-18 01:08:29 -0400

    Added app routes

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -10,7 +10,27 @@
             .state('dashboard', {
                 url: '/',
                 templateUrl: 'js/components/dashboard/views/dashboard.html'
-            });
+            })
+
+            .state('savings', {
+                url: '/savings',
+                templateUrl: 'js/components/savings/views/savings.html'
+            })
+
+            .state('expenses', {
+                url: '/expenses',
+                templateUrl: 'js/components/expenses/views/expenses.html'
+            })
+
+            .state('profile', {
+                url: '/profile',
+                templateUrl: 'js/components/profile/views/profile.html'
+            })
+
+            .state('friends', {
+                url: '/friends',
+                templateUrl: 'js/components/friends/views/friends.html'
+            })
 
         $urlRouterProvider.otherwise('/');
         $locationProvider.html5Mode(true);
