c69e6a5b3501f984631f1347cbfd4275ff3f5e27
yazman12 <yazman12@gmail.com>
2016-03-24 18:48:48 -0400

    Redoing frontend foundation

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -23,27 +23,6 @@
                 templateUrl: 'js/components/register/views/register.html'
             })
 
-            .state('savings', {
-                url: '/savings',
-                templateUrl: 'js/components/savings/views/savings.html'
-            })
-
-            .state('expenses', {
-                url: '/expenses',
-                templateUrl: 'js/components/expenses/views/expenses.html',
-                controller: 'expensesCtrl'
-            })
-
-            .state('profile', {
-                url: '/profile',
-                templateUrl: 'js/components/profile/views/profile.html'
-            })
-
-            .state('friends', {
-                url: '/friends',
-                templateUrl: 'js/components/friends/views/friends.html'
-            })
-
         $urlRouterProvider.otherwise('/');
         $locationProvider.html5Mode(true);
     }
