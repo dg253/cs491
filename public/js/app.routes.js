d9cca5c7ede069358bd14a6a074dfafd531977e1
yazman12 <yazman12@gmail.com>
2016-03-26 15:11:13 -0400

    Added routes

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -9,18 +9,41 @@
         $stateProvider
             .state('overview', {
                 url: '/',
-                templateUrl: 'js/components/overview/views/overview.html',
+                templateUrl: 'js/components/overview/overview.html',
                 controller: 'overviewCtrl'
+            })
+
+            .state('expenses', {
+                url: '/expenses',
+                templateUrl: 'js/components/expenses/expenses.html'
+            })
+
+            .state('savings', {
+                url: '/savings',
+                templateUrl: 'js/components/savings/savings.html'
+
+            })
+
+            .state('transactions', {
+                url: '/transactions',
+                templateUrl: 'js/components/transactions/transactions.html'
+
+            })
+
+            .state('wishlist', {
+                url: '/wishlist',
+                templateUrl: 'js/components/wishlist/wishlist.html'
+
             })
 
             .state('login', {
                 url: '/login',
-                templateUrl: 'js/components/login/views/login.html'
+                templateUrl: 'js/components/login/login.html'
             })
 
             .state('register', {
                 url: '/register',
-                templateUrl: 'js/components/register/views/register.html'
+                templateUrl: 'js/components/register/register.html'
             })
 
         $urlRouterProvider.otherwise('/');
