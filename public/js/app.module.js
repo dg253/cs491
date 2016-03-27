3a29a6d0b00d12bef251e31658395112a041e295
yazman12 <yazman12@gmail.com>
2016-03-27 13:36:39 -0400

    Updated expenses page

--- public/js/app.module.js
+++ public/js/app.module.js
@@ -2,5 +2,6 @@
     angular
         .module('savery', ['ui.router', 'chart.js']);
 
-    Chart.defaults.global.responsive = true;
+    Chart.defaults.global.responsive = false;
+    Chart.defaults.global.colours = ['#40557D', '#336FAF', '#FFD400', '#F0B499', '#72C4B9', '#289DF5'];
 }());
--- public/js/components/expenses/expenses.controller.js
+++ public/js/components/expenses/expenses.controller.js
@@ -6,8 +6,19 @@
     expensesCtrl.$inject = ['$scope'];
 
     function expensesCtrl ($scope) {
+        $scope.sum = 0;
+
         $scope.labels = ['Bills', 'Clothes', 'Transportation', 'Entertainment', 'Gas', 'Other'];
         $scope.data = [39, 14, 13, 12, 6, 16];
-        $scope.showLegend = true;
+        $scope.colors = Chart.defaults.global.colours;
+        console.log($scope.colors);
+
+        $scope.data.forEach(function (num, index) {
+            $scope.sum += num;
+        });
+
+        $scope.chartOptions = {
+            percentageInnerCutout : 70
+        };
     }
 }());
