27e94228cc726874be09359a0f4076b3b19aa054
yazman12 <yazman12@gmail.com>
2016-03-27 15:27:14 -0400

    Updated expenses page

--- public/js/app.module.js
+++ public/js/app.module.js
@@ -3,5 +3,5 @@
         .module('savery', ['ui.router', 'chart.js']);
 
     Chart.defaults.global.responsive = false;
-    Chart.defaults.global.colours = ['#40557D', '#336FAF', '#FFD400', '#F0B499', '#72C4B9', '#289DF5'];
+    Chart.defaults.global.colours = ['#1f96ef', '#2ecc71', '#3471b0', '#FFD400', '#f0b499', '#40557d'];
 }());
