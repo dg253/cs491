ae5f81303308c39148ca9053d89b7ffec2712fd6
yazman12 <yazman12@gmail.com>
2016-03-30 19:19:36 -0400

    Added progress circles and directive

---
+++ public/js/components/common/progressbar.directive.js
@@ -0,0 +1,47 @@
+(function () {
+    angular
+        .module('savery')
+        .directive('progressCircle', progressCircle);
+
+    function progressCircle () {
+        var directive = {
+            restrict: 'A',
+            replace: true,
+            link: link
+        };
+
+        function link ($scope, iElm, iAttrs) {
+            var startColor = iAttrs.startColor || '#FC5B3F';
+            var endColor = iAttrs.endColor || '#295889';
+
+            var beginAt = 0;
+            var stopAt = iAttrs.stopat;
+
+            var element = iElm[0];
+            var circle = new ProgressBar.Circle(element, {
+                color: startColor,
+                trailColor: '#eee',
+                trailWidth: 1,
+                duration: 2000,
+                easing: 'bounce',
+                strokeWidth: 8,
+
+                // Set default step function for all animate calls
+                step: function(state, circle) {
+                    circle.path.setAttribute('stroke', state.color);
+
+                    if (beginAt++ < stopAt) {
+                        circle.setText(beginAt + '%');
+                    }
+                }
+            });
+
+            circle.animate((stopAt / 100), {
+                from: {color: startColor},
+                to: {color: endColor}
+            });
+        }
+
+        return directive;
+    }
+}());
