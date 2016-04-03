ef13ce01fe7e02512bb7827727a7936c298db1a5
yazman12 <yazman12@gmail.com>
2016-04-03 14:14:20 -0400

    Added pagination to bills table

--- public/js/components/expenses/expenses.controller.js
+++ public/js/components/expenses/expenses.controller.js
@@ -11,7 +11,6 @@
         $scope.labels = ['Bills', 'Clothes', 'Transportation', 'Entertainment', 'Gas', 'Other'];
         $scope.data = [39, 14, 13, 12, 6, 16];
         $scope.colors = Chart.defaults.global.colours;
-        console.log($scope.colors);
 
         $scope.data.forEach(function (num, index) {
             $scope.sum += num;
