47f1cabcf036085fa7035a0fea724441cd644522
justdave <drg23@njit.edu>
2016-04-04 23:31:58 -0400

    overview changes

--- public/js/components/overview/overview.controller.js
+++ public/js/components/overview/overview.controller.js
@@ -6,6 +6,19 @@
     overview.$inject = ['$scope'];
 
     function overview ($scope) {
+
+
+      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+      $scope.series = ['Series A', 'Series B'];
+      $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ];
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
+
+
         $scope.Line = {
             data: {
                 labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
