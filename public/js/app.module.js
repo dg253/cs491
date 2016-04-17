b8f30d85a6bbe7fd3d552f97a6853bef503cb580
matthew mccann <matthew mccann>
2016-04-17 12:31:26 -0400

    changed graph layout on resize, added ng-animate dependanct for calander

--- public/js/app.module.js
+++ public/js/app.module.js
@@ -1,6 +1,6 @@
 (function () {
     angular
-        .module('savery', ['ui.router', 'chart.js']);
+        .module('savery', ['ui.router', 'chart.js', 'ngAnimate', 'calendarApp']);
 
     Chart.defaults.global.responsive = false;
     Chart.defaults.global.colours = ['#1f96ef', '#2ecc71', '#3471b0', '#FFD400', '#f0b499', '#40557d'];
