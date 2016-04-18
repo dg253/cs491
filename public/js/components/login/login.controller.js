c19a85fb7fbb5c07e75c17e0a680b1abc8c4664e
yazman12 <yazman12@gmail.com>
2016-04-17 22:57:05 -0400

    Added login page

--- public/js/components/login/login.controller.js
+++ public/js/components/login/login.controller.js
@@ -6,11 +6,32 @@
     loginCtrl.$inject = ['$scope', '$state'];
 
     function loginCtrl ($scope, $state) {
-        $scope.login = function () {
-            localStorage.loggedIn = true;
-            //$state.go('overview');
-            location.reload();
-            console.log('logged in');
+        $scope.login = function (e) {
+            console.log(arguments);
+
+            var working = false;
+            e.preventDefault();
+
+            if (working) return;
+
+            working = true;
+
+            var $this = $('.login'),
+                $state1 = $this.find('button > .state');
+            $this.addClass('loading');
+            $state1.html('Authenticating');
+            setTimeout(function() {
+                $this.addClass('ok');
+                $state1.html('Welcome back!');
+                setTimeout(function() {
+                    $this.removeClass('ok loading');
+                    working = false;
+
+                    localStorage.loggedIn = true;
+                    $state.go('overview');
+                    location.reload();
+                }, 1500);
+            }, 1500);
         };
     }
 }());
