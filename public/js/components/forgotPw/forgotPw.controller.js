b064bd4720421fdac73cdc3b06c14c6bbf794b0a
yazman12 <yazman12@gmail.com>
2016-04-29 17:59:23 -0400

    Added reset password

--- public/js/components/forgotPw/forgotPw.controller.js
+++ public/js/components/forgotPw/forgotPw.controller.js
@@ -3,9 +3,17 @@
         .module('savery')
         .controller('forgotCtrl', forgotCtrl);
 
-    forgotCtrl.$inject = ['$scope'];
+    forgotCtrl.$inject = ['$scope', '$state'];
 
-    function forgotCtrl ($scope) {
-        console.log('forgot');
+    function forgotCtrl ($scope, $state) {
+        $scope.resetPw = function () {
+            swal({
+                title: 'Check your email',
+                text: 'Instructions on resetting your password have been sent',
+                imageUrl: '/assets/img/flat-email-icon.png'
+            }, function () {
+                $state.go('login');
+            });
+        };
     }
 }());
