2cd23e8ca4f63d1dd41bb31aa4dc1fdbeb05c8f9
David <dg253@njit.edu>
2016-05-01 17:18:58 -0400

    -removed ngclipboard

--- public/js/app.module.js
+++ public/js/app.module.js
@@ -1,6 +1,6 @@
 (function () {
     angular
-        .module('savery', ['ui.router', 'chart.js', 'ngAnimate', 'calendar', 'ngclipboard']);
+        .module('savery', ['ui.router', 'chart.js', 'ngAnimate', 'calendar']);
 
     Chart.defaults.global.responsive = false;
     Chart.defaults.global.colours = ['#1f96ef', '#2ecc71', '#3471b0', '#FFD400', '#f0b499', '#40557d'];
