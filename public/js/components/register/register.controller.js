c8457829c6554f0c2c642cb6b947d1f11e41626c
yazman12 <yazman12@gmail.com>
2016-05-03 14:00:35 -0400

    Added registration

--- public/js/components/register/register.controller.js
+++ public/js/components/register/register.controller.js
@@ -1,6 +1,38 @@
 (function () {
     angular
         .module('savery')
+        .controller('registerCtrl', loginCtrl);
+
+    loginCtrl.$inject = ['$scope', '$state'];
+
+    function loginCtrl ($scope, $state) {
+        $scope.register = function (e) {
+
+            var working = false;
+            e.preventDefault();
+
+            var $this = $('.login'),
+                $state1 = $this.find('button > .state');
+            $this.addClass('loading');
+            $state1.html('Initializing');
+            setTimeout(function() {
+                $this.addClass('ok');
+                $state1.html('Redirecting to login');
+                setTimeout(function() {
+                    $this.removeClass('ok loading');
+                    working = false;
+
+                    $state.go('login');
+                }, 1500);
+            }, 1500);
+        };
+    }
+}());
+
+/*
+(function () {
+    angular
+        .module('savery')
         .controller('registerCtrl', registerCtrl);
 
     registerCtrl.$inject = ['$scope', '$state'];
@@ -17,4 +49,4 @@
             localStorage.reloaded = true;
         }
     }
-}());
+}());*/
